6f91956137a835914720d4b22dda29cf268eede6
vibi <vibi94@gmail.com>
2019-10-17 12:53:54 +0700

    CM-27 feat(cm): First commit for category manager

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+
+// call router in folder routes
+const mainRouter = require('./routes')
+app.use('/', mainRouter)
+
+app.use((_req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+});
+
+// jalankan nodejs di port sesuai ENV
+if(process.env.PORT) {
+    app.listen(process.env.PORT, () => {
+        console.log('server running at port', process.env.PORT)
+    })
+}
+module.exports = app;
